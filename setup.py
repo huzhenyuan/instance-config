7555c208103e5d8b09f95e78661e8f7cba50f6d4
rory <rory@orqus.io>
2026-05-13 19:22:59 +0800

    123

--- setup.py
+++ setup.py
@@ -291,7 +291,7 @@ class InstanceAgent:
         gpu_name: str = "",
     ):
         self._server = server_url.rstrip("/")
-        self._instance_id = os.getenv("CONTAINER_ID", "") or f"stable-{os.getenv('HOSTNAME', '')}"
+        self._instance_id = os.getenv("CONTAINER_ID", "")
         self._group_name = group_name
         self._gpu_name = gpu_name or os.getenv("GPU_NAME", "")
         self._public_ip = os.getenv("PUBLIC_IPADDR", "")
